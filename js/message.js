3788753ee21e40ac5ab8676e689a30472833c50c
more-ka <1640627628@qq.com>
2019-04-28 20:19:42 +0800

    “使用MVC封装”

--- js/message.js
+++ js/message.js
@@ -1,36 +1,68 @@
-var APP_ID = '9HOQuYpxQkbcsC81qKRHnKv3-gzGzoHsz';
-var APP_KEY = 'NJr4FD0aO0zM4Pr48hM5VDcu';
-
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
-var query = new AV.Query('Message');
-query.find().then(function (information) {
-  var array = information.map((item)=>item.attributes)
-  array.forEach((item)=>{
-    let li = document.createElement('li')
-    li.innerHTML = `${item.name}:  ${item.content}`
-    let messageList = document.querySelector('#messageList')
-    messageList.append(li)
-  })
-  })
- 
-var myForm = document.querySelector('#messageForm')
-myForm.addEventListener('submit',(e)=>{
-  e.preventDefault()
-  let content = myForm.querySelector(['input[name=content]']).value
-  let name = myForm.querySelector('input[name=name]').value
-  var message = AV.Object.extend('Message');
-  var message = new message(); message.save({
-    'name':name,
-    'content': content
-  }).then(function(object) {
-    let li = document.createElement('li')
-    li.innerHTML = `${object.attributes.name}:  ${object.attributes.content}`
-    let messageList = document.querySelector('#messageList')
-    messageList.append(li)
-    myForm.querySelector(['input[name=content]']).value = ''
-    myForm.querySelector(['input[name=name]']).value = ''
-  })
-})
+!function(){
+  let view = document.querySelector('.message')
+  let model = {
+    init:function(){
+      var APP_ID = '9HOQuYpxQkbcsC81qKRHnKv3-gzGzoHsz'
+      var APP_KEY = 'NJr4FD0aO0zM4Pr48hM5VDcu'      
+      AV.init({ appId: APP_ID, appKey: APP_KEY })
+    },
+    fetch:function(){
+      var query = new AV.Query('Message');
+      return query.find()
+    },
+    save: function(name,content){
+      var message = AV.Object.extend('Message');
+        var message = new message(); 
+        if(name !== '' && content !== ''){return message.save({
+          'name':name,
+          'content': content
+        })
+        
+    }
+    }
+  }
+  let contorller = {
+    view:null,
+    model:null,
+    init:function(view,model){
+      this.view = view
+      this.model = model
+      this.model.init()
+      this.readData()
+      this.form = view.querySelector('#messageForm')
+      this.bindEvents()
+      this.saveData()
+    },
+    readData:function(){      
+      model.fetch().then(function (information) {
+        var array = information.map((item)=>item.attributes)
+        array.forEach((item)=>{
+          let li = document.createElement('li')
+          li.innerHTML = `${item.name}:  ${item.content}`
+          let messageList = view.querySelector('#messageList')
+          messageList.append(li)
+        })
+        })
+    },
+    bindEvents:function(){
+      this.form.addEventListener('submit',(e)=>{
+        e.preventDefault()
+        this.saveData()
+      })
+    },
+    saveData: function(){
+      let myForm = this.form
+      let content = myForm.querySelector(['input[name=content]']).value
+        let name = myForm.querySelector('input[name=name]').value
+        this.model.save(name,content).then((object)=>{
+          let li = document.createElement('li')
+          li.innerHTML = `${object.attributes.name}:  ${object.attributes.content}`
+          let messageList = document.querySelector('#messageList')
+          messageList.append(li)
+          myForm.querySelector(['input[name=content]']).value = ''
+          myForm.querySelector(['input[name=name]']).value = ''
+        })}
+    }
+  
+  contorller.init.call(contorller,view,model)
+}.call()
